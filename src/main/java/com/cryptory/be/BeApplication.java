69c57b77e21da1660c5168fc3744c2aa7a0911ae
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-31 00:17:18 +0900

    Fix: SPRING_PROFILES_ACTIVE는 스프링 디폴트값이여서 다른 변수로 변경

--- src/main/java/com/cryptory/be/BeApplication.java
+++ src/main/java/com/cryptory/be/BeApplication.java
@@ -11,8 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BeApplication {
 	public static void main(String[] args) {
 		// 운영 환경이 아니면 .env 파일 로드
-		String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
-		if (activeProfile == null || !activeProfile.equals("prod")) {
+		String activeProfile = System.getenv("ENV_ACTIVE");
+		if (activeProfile == null || !activeProfile.equals("NO")) {
 			Dotenv dotenv = Dotenv.load();
 			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		}

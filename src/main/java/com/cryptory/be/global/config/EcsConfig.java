2ba884ecda481c98225d1f1be6b63f6899206a76
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-31 16:19:25 +0900

    Fix: AWSMetadataTemplate을 사용하여 public IP 받아오도록 수정

--- src/main/java/com/cryptory/be/global/config/EcsConfig.java
+++ src/main/java/com/cryptory/be/global/config/EcsConfig.java
@@ -1,5 +1,6 @@
 package com.cryptory.be.global.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
@@ -13,11 +14,21 @@ import java.net.UnknownHostException;
 @Configuration
 @Slf4j
 @Profile("docker")
+@RequiredArgsConstructor
 public class EcsConfig {
+    private AwsMetadataTemplate awsMetadataTemplate;
+
     @Bean
     public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
         EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
         String ip = null;
+        String awsToken = awsMetadataTemplate.createToken();
+        if (awsToken != null) {
+            ip = awsMetadataTemplate.getPublicIp(awsToken);
+        }
+        if (ip == null) {
+            config.setHostname(ip);
+        }
         try{
             ip = InetAddress.getLocalHost().getHostAddress();
             log.info("ECS Task Container Private Ip address is {}", ip);
